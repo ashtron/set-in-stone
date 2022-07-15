6a2d7dd9b827449f3b9acb1d4fd1acfe9a4d7be7
ashtron <ashton.playsted@protonmail.com>
2022-07-15 01:43:40 -0700

    Add more tests

--- test/SetInStone.js
+++ test/SetInStone.js
@@ -25,15 +25,32 @@ describe("SetInStone contract", function() {
         expect(pactAddress).to.equal(signer2.address)
     })
 
-    // it("Should allow pacts to be confirmed", async function() {
-    //     const { SetInStone, signer1, signer2 } = await loadFixture(deployTokenFixture)
-    //     console.log(await SetInStone.getPact("0"))
+    it("Should allow pacts to be confirmed", async function() {
+        const { SetInStone, signer2 } = await loadFixture(deployTokenFixture)
+        
+        const description = "We hereby solemnly swear to..."
+        const taker = signer2.address
 
-    //     // await SetInStone.connect(signer2).confirmPact(0)
+        await SetInStone.createPact(description, taker)
+        await SetInStone.connect(signer2).confirmPact(0)
 
-    //     // console.log(console.log(await SetInStone.getPact(0)))
-    // })
+        const status = Array.from(await SetInStone.getPact(0))[3]
 
-    // it("Should only allow the taker to confirm a pact", async function() {
-    // })
+        // enum values are returned as integers.
+        expect(status).to.equal(1)
+    })
+
+    it("Should only allow the taker to confirm a pact", async function() {
+        const { SetInStone, signer2 } = await loadFixture(deployTokenFixture)
+        
+        const description = "We hereby solemnly swear to..."
+        const taker = signer2.address
+
+        await SetInStone.createPact(description, taker)
+
+        await expect(SetInStone.confirmPact(0)).to.be.reverted
+
+        const status = Array.from(await SetInStone.getPact(0))[3]
+        expect(status).to.equal(0)
+    })
 })
