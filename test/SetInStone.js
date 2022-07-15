9b78df7ec260ba633d6b722541ca45e562c38599
ashtron <ashton.playsted@protonmail.com>
2022-07-15 01:59:49 -0700

    Refactor tests

--- test/SetInStone.js
+++ test/SetInStone.js
@@ -12,30 +12,25 @@ describe("SetInStone contract", function() {
         return { SetInStoneFactory, SetInStone, signer1, signer2 }
     }
 
+    async function createPact(contract, description, taker) {
+        await contract.createPact(description, taker)
+    }
+
     it("Should create a new pact", async function() {
         const { SetInStone, signer2 } = await loadFixture(deployTokenFixture)
 
-        const description = "We hereby solemnly swear to..."
-        const taker = signer2.address
-
-        await SetInStone.createPact(description, taker)
-
+        createPact(SetInStone, "We hereby solemnly swear to...", signer2.address)
         const pactAddress = Array.from(await SetInStone.getPact(0))[2]
-
         expect(pactAddress).to.equal(signer2.address)
     })
 
     it("Should allow pacts to be confirmed", async function() {
         const { SetInStone, signer2 } = await loadFixture(deployTokenFixture)
         
-        const description = "We hereby solemnly swear to..."
-        const taker = signer2.address
-
-        await SetInStone.createPact(description, taker)
+        createPact(SetInStone, "We hereby solemnly swear to...", signer2.address)
         await SetInStone.connect(signer2).confirmPact(0)
 
         const status = Array.from(await SetInStone.getPact(0))[3]
-
         // enum values are returned as integers.
         expect(status).to.equal(1)
     })
@@ -43,11 +38,7 @@ describe("SetInStone contract", function() {
     it("Should only allow the taker to confirm a pact", async function() {
         const { SetInStone, signer2 } = await loadFixture(deployTokenFixture)
         
-        const description = "We hereby solemnly swear to..."
-        const taker = signer2.address
-
-        await SetInStone.createPact(description, taker)
-
+        createPact(SetInStone, "We hereby solemnly swear to...", signer2.address)
         await expect(SetInStone.confirmPact(0)).to.be.reverted
 
         const status = Array.from(await SetInStone.getPact(0))[3]
