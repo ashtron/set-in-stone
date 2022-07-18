d5c6cc8025995a9b4e64337a424556c3d6f6d2f5
ashtron <ashton.playsted@protonmail.com>
2022-07-17 17:38:13 -0700

    Add state to NewPactForm

--- frontend/src/components/NewPactForm.js
+++ frontend/src/components/NewPactForm.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import "bulma/css/bulma.min.css"
 import { Hero, Container, Box, Button, Field, Label, Form } from "react-bulma-components"
 
 export function NewPactForm({ createPact }) {
+    const [description, setDescription] = useState("")
+    const [address, setAddress] = useState("")
+
+    function handleDescriptionChange(event) {
+        setDescription(event.target.value)
+    }
+
+    function handleAddressChange(event) {
+        setAddress(event.target.value)
+    }
+
     return (
         <div>
             <Hero size="fullheight">
@@ -12,18 +23,26 @@ export function NewPactForm({ createPact }) {
                             <Form.Field>
                                 <Form.Label>Description</Form.Label>
                                 <Form.Control>
-                                    <Form.Input type="text" placeholder="We solemnly swear that..." />
+                                    <Form.Input
+                                        type="text"
+                                        placeholder="We solemnly swear that..."
+                                        onChange={ handleDescriptionChange }
+                                    />
                                 </Form.Control>
                             </Form.Field>
 
                             <Form.Field>
                                 <Form.Label>Taker Address</Form.Label>
                                 <Form.Control>
-                                    <Form.Input type="text" placeholder="0x9bEa9..." />
+                                    <Form.Input
+                                        type="text"
+                                        placeholder="0x9bEa9..."
+                                        onChange={ handleAddressChange }
+                                    />
                                 </Form.Control>
                             </Form.Field>
 
-                            <Button color="grey-light" onClick={ () => {
+                            <Button color="grey-light" onClick={() => {
                                 createPact()
                             }}>Submit</Button>
                         </Box>
