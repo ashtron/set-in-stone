3e20ccabf4156f2988afce69ecb2292d05e46144
ashtron <ashton.playsted@protonmail.com>
2022-07-26 14:13:02 -0700

    Display a message if user has no pacts

--- frontend/src/components/Pacts.js
+++ frontend/src/components/Pacts.js
@@ -3,9 +3,19 @@ import { PactViewsContext } from "./Dapp"
 import { Pact } from "./Pact"
 
 import { Link } from "react-router-dom"
+import { Button } from "react-bulma-components"
 
 export function Pacts() {
     const pacts = useContext(PactViewsContext)._pacts
+
+    const noPactsMessage =
+        <div>
+            <h3>You haven't made any pacts yet!</h3>
+            
+            <Button style={{ marginTop: "1rem" }}>
+                <Link to="/pacts/new">Create a Pact</Link>
+            </Button>
+        </div>
     
     const pactComponents = pacts.map(pact => {
         return (
@@ -19,7 +29,7 @@ export function Pacts() {
 
     return (
         <div>
-            {pactComponents}
+            { pactComponents.length === 0 ? noPactsMessage : pactComponents }
         </div>
     )
 }
